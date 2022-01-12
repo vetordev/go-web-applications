6014a7b55cce3dfb7b0725a7f892466cbcee3551
vetordev <vitor13m2003@gmail.com>
2022-01-12 00:18:45 -0300

    refactor(gowiki): project structure

---
+++ gowiki/server/controller.go
@@ -0,0 +1,63 @@
+package server
+
+import (
+	"net/http"
+)
+
+func ViewController(w http.ResponseWriter, r *http.Request) {
+	method := r.Method
+
+	var handler http.HandlerFunc
+
+	switch method {
+	case http.MethodGet:
+		handler = makeHandler(ViewService)
+	default:
+		http.NotFound(w, r)
+	}
+
+	handler(w, r)
+}
+
+func EditController(w http.ResponseWriter, r *http.Request) {
+	method := r.Method
+
+	var handler http.HandlerFunc
+
+	switch method {
+	case http.MethodGet:
+		handler = makeHandler(EditService)
+	default:
+		http.NotFound(w, r)
+	}
+
+	handler(w, r)
+}
+
+func SaveController(w http.ResponseWriter, r *http.Request) {
+	method := r.Method
+
+	var handler http.HandlerFunc
+
+	switch method {
+	case http.MethodPost:
+		handler = makeHandler(SaveService)
+	default:
+		http.NotFound(w, r)
+	}
+
+	handler(w, r)
+}
+
+func makeHandler(service Service) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		title, err := getTitle(r.URL.Path)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		service(w, r, title)
+	}
+}
